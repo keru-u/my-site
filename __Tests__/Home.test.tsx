798aa7b9509ab13dab2d4c427677736a5d00eea2
keru-u <keru-u@users.noreply.github.com>
2022-01-23 22:20:51 +0900

    初回コミットです。

---
+++ __Tests__/Home.test.tsx
@@ -0,0 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+ import { render, screen } from '@testing-library/react'
+ import '@testing-library/jest-dom/extend-expect'
+ import Home from '../pages/index'
+ 
+ it('Should render title text', () => {
+   render(<Home />)
+ })
