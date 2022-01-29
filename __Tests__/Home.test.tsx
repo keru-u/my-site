75b3d6b41b76664dfcc0083584f752154f77e04c
keru-u <keru-u@users.noreply.github.com>
2022-01-29 22:36:55 +0900

    ディレクトリ構造の変更に合わせてJestの設定も変更しました。

--- __Tests__/Home.test.tsx
+++ __Tests__/Home.test.tsx
@@ -3,7 +3,7 @@
  */
  import { render, screen } from '@testing-library/react'
  import '@testing-library/jest-dom/extend-expect'
- import Home from '../pages/index'
+ import Home from '../src/pages/index'
  
  it('Should render title text', () => {
    render(<Home />)
