835309724d913d0b340aa6ef8543557c98968ed6
keru-u <keru-u@users.noreply.github.com>
2022-02-06 23:42:09 +0900

    こちらではコミットしてないけどGASの方をいじりました。

--- src/pages/isekityosa-matome.tsx
+++ src/pages/isekityosa-matome.tsx
@@ -3,7 +3,9 @@ import Layout from '../../components/Layout';
 const Iseki = () => {
     return (
         <Layout title='けるのうたわれ倉庫 遺跡調査まとめ' description='工事中につきコンテンツはありません。'>
-        <div>遺跡調査まとめ</div>
+        <div>
+            遺跡調査まとめ
+        </div>
         </Layout>
 )}
 
