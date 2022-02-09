8416e5b775e8acabfcaf48ef7a18e9ae16f1bfbf
keru-u <keru-u@users.noreply.github.com>
2022-02-09 12:40:50 +0900

    不要なファイルを削除しました。

--- src/pages/blog-page.tsx
+++ src/pages/blog-page.tsx
@@ -2,7 +2,7 @@ import Layout from '../../components/Layout';
 
 const Blog = () => {
     return (
-        <Layout title='けるのうたわれ倉庫 ブログ' description='工事中につきコンテンツはありません。'>
+        <Layout title='ブログ' description='工事中につきコンテンツはありません。'>
         <div>Blog page</div>
         </Layout>
     )}
--- src/pages/contact-page.tsx
+++ src/pages/contact-page.tsx
@@ -2,7 +2,7 @@ import Layout from '../../components/Layout';
 
 const Contact = () => {
     return (
-        <Layout title='けるのうたわれ倉庫 連絡先' description='工事中につきコンテンツはありません。'>
+        <Layout title='連絡先' description='工事中につきコンテンツはありません。'>
         <div>Contact Page</div>
         </Layout>
 )}
