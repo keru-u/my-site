ed77568e9b8b8f5bb63466d9fb0e2cfc4f921f10
keru-u <keru-u@users.noreply.github.com>
2022-02-20 22:21:06 +0900

    なんかTypeScriptのエラー出てるけど動く。

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,37 +1,18 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import Head from 'next/head'
 import { Box, Container, ChakraProvider } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 import theme from './theme'
 import { Work } from '../molecules/Work'
+import { HeadEx } from '../molecules/HeadEx'
 
 type LayoutProps = {
   children?: ReactNode
-  title: string
-  description: string
 }
 
-export const Layout = ({ children, title, description }: LayoutProps) => {
+export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7d9661" />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Reggae+One&display=optional"
-        />
-        <meta name="msapplication-TileColor" content="#7d9661" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-      </Head>
+      <HeadEx />
       <main>
         <ChakraProvider theme={theme}>
           <Container maxW="container.md">
