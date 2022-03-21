6abef56238f892e6ddc31529948ded111a2461fb
keru-u <keru-u@users.noreply.github.com>
2022-03-21 23:04:55 +0900

    不要なコメントを削除しました。

--- src/pages/iseki.tsx
+++ src/pages/iseki.tsx
@@ -1,34 +1,16 @@
 import type { NextPage } from 'next'
 
 import { Box } from '@chakra-ui/react'
-// import axios from 'axios'
 
 import Layout from '../components/templates/Layout'
 import { SiteTopStatus } from '../components/organisms/SiteTopStatus'
 import { HeadingEx } from '../components/molecules/HeadingEx'
 import { Work } from '../components/molecules/Work'
 
-// type IsekiTokei = {
-//   id: number
-//   name: string
-//   to20Count: number
-//   to24Count: number
-//   to29Count: number
-//   over30Count: number
-//   to20Rate: number
-//   to24Rate: number
-//   to29Rate: number
-//   over30Rate: number
-// }
-
 const Iseki: NextPage = () => {
   const title: string = '遺跡調査報酬 統計調査'
   const description: string = '現在工事中です。'
   const src: string = '/images/avatar.png'
-  // const endpoint: string ='https://script.google.com/macros/s/AKfycby8JapKqwvjNQug7zUlTQJm4ulzYjaqTaKv8mmT-UGYMdfT-XH1TlkbghBn9cqxEwsF/exec'
-
-  // const res = await fetch(endpoint,{redirect: 'follow'})
-  // const json: IsekiTokei =await res.json()
 
   return (
     <Layout title={title} description={description}>
